3518c04cba18c06e6ca382117fe564f8488b021c
Trevor Recker <trevorrecker@users.noreply.github.com>
2026-05-06 17:47:48 -0500

    add support for script and style block syntax highlighting

--- languages/pug/injections.scm
+++ languages/pug/injections.scm
@@ -1,1 +1,16 @@
-((javascript) @content (#set! "language" "javascript"))
+(tag
+  (tag_name) @_tag_name
+  (#eq? @_tag_name "script")
+  (children) @injection.content
+  (#set! injection.language "javascript")
+  (#set! injection.include-children))
+
+(tag
+  (tag_name) @_tag_name
+  (#eq? @_tag_name "style")
+  (children) @injection.content
+  (#set! injection.language "css")
+  (#set! injection.include-children))
+
+((javascript) @injection.content
+  (#set! injection.language "javascript"))
